6a6c5901fd995507bd2f2ca3c7473044e5cfa5d8
Andy Bolton <boltonandrew@live.co.uk>
2022-07-07 12:14:36 +0100

    ref

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../user/model");
 
 exports.hashPass = async (req, res, next) => {
   try {
@@ -7,6 +9,36 @@ exports.hashPass = async (req, res, next) => {
     // req.body.password = hashedPass; //stores freshly hashed password back in the req body
     req.body.password = await bcrypt.hash(req.body.password, 8); //all steps above, condensed into 1 line
     next(); //moves onto next middleware/controller in endpoint
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+exports.comparePass = async (req, res, next) => {
+  try {
+    req.user = await User.findOne({ username: req.body.username });
+    // const match = await bcrypt.compare(req.body.password, user.password);
+    if (await bcrypt.compare(req.body.password, req.user.password)) {
+      next();
+    } else {
+      throw new Error("Incorrect credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+exports.tokenCheck = async (req, res, next) => {
+  try {
+    // const token = req.header("Authorization"); //grab token from Authorization header in the request
+    const decodedToken = jwt.verify(
+      req.header("Authorization"),
+      process.env.SECRET
+    ); //decode token using same secret that created the token
+    req.user = await User.findById(decodedToken.id); //finding the user by their id, stored in the token
+    next();
   } catch (error) {
     console.log(error);
     res.send({ error });
