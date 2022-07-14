ce1f30386e2d92d3d40012ede3b2b1efd92f800b
Andy Bolton <boltonandrew@live.co.uk>
2022-07-14 14:50:22 +0100

    ref

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -18,25 +18,11 @@ exports.login = async (req, res) => {
     //   username: req.body.username,
     //   password: req.body.password,
     // });
-    console.log("in login " + req.user);
     if (!req.user) {
       throw new Error("Incorrect credentials");
     } else {
-      res.send({ user: req.user });
-    }
-  } catch (error) {
-    console.log(error);
-    res.send({ error });
-  }
-};
-
-exports.listUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.params.username });
-    if (!user) {
-      throw new Error("No user found");
-    } else {
-      res.send({ user });
+      const token = jwt.sign({ id: req.user._id }, process.env.SECRET);
+      res.send({ user: req.user, token });
     }
   } catch (error) {
     console.log(error);
@@ -46,12 +32,26 @@ exports.listUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   try {
-    const result = await User.findOneAndUpdate(
+    const result = await User.updateOne(
       req.body.filterObj,
       req.body.updateObj
-    );
+    ); /*{username: "AndyB"} {keyName: "value"}*/
     if (result.modifiedCount > 0) {
       res.status(200).send({ msg: "Successfully Updated" });
+    } else {
+      throw new Error({ msg: "Something went wrong" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const result = await User.deleteOne({ _id: req.user._id });
+    if (result.deletedCount > 0) {
+      res.send({ msg: "Successfully Deleted" });
     } else {
       throw new Error({ msg: "Something went wrong" });
     }
